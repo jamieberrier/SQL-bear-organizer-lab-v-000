3df1dd057a59ad9e7c028f51e389f12b1caf5257
jamieberrier <berrier.jamie@gmail.com>
2019-05-31 19:42:58 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,3 +6,6 @@ VALUES ("Rowdy", 1, "M", "Black", "Playful", 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Tabitha", 2, "F", "Black", "Playful", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 10, "M", "Grizzly", "Serious", 1);
